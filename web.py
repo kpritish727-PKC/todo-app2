ea822f2e370bf3ada03ecd179bd52ceb5c952e91
pritish <kpritish727@gmail.com>
2026-04-26 17:30:58 -0400

    New Initial commit

--- web.py
+++ web.py
@@ -4,14 +4,15 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] = "\n"
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
-st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
 st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity")
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -19,11 +20,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
-
-print("What's Up?")
-
-st.session_state
